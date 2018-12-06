800b825c34510e67bb198532603956dae49127ce
yaohuijiang1 <44475404+yaohuijiang1@users.noreply.github.com>
2018-12-06 11:10:51 -0700

    all files

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+var app = express();
+
+app.use("/", express.static(__dirname + "public"));
+
+app.use(function (req, res, next){
+    res.setHeader('Access-Control-Allow_Orign', '*');
+    res.setHeader('Access-Control-Allow_Methods', 'Get');
+    res.setHeader('Access-Control-Allow_Headers', 'X-Requested-Wish,content-type');
+    next();
+});
+
+var server = app.listen(process.env.PORT || 8080, function(){
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
