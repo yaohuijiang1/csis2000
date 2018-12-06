e10454215b76cfa246d820f52fb13b2abc183fa6
yaohuijiang1 <44475404+yaohuijiang1@users.noreply.github.com>
2018-12-06 11:21:25 -0700

    updages file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 
 var app = express();
 
-app.use("/", express.static(__dirname + "public"));
+app.use("/", express.static("public"));
 
 app.use(function (req, res, next){
     res.setHeader('Access-Control-Allow_Orign', '*');
